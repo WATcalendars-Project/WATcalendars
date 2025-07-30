0ef8ff85b815e2dd4498d9463e986853e7fb3889
Dominik Serafin <serafin652002@gmail.com>
2025-07-30 13:13:27 +0200

    add color definitions and log formats for console output

---
+++ src/watcalendars/logutils.py
@@ -0,0 +1,14 @@
+# Definicja kolorow do printa
+# uzycie:
+# print(f"{RED}kolor czerwony{RESET}")
+RED = '\033[91m'
+GREEN = '\033[92m'
+YELLOW = '\033[93m'
+BLUE = '\033[94m'
+RESET = '\033[0m'
+
+# Zdefiniowane logi
+E = f"{RED}E{RESET}:"
+W = f"{YELLOW}W{RESET}:"
+INFO = f"[{BLUE}INFO{RESET}]:"
+OK = f"{GREEN}OK{RESET}:"
